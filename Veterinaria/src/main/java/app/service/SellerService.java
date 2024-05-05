a7568e403f2e7a103f1b8ff49dd245c3040c9f20
TDAjoansantamaria <joan.santamaria@correo.tdea.edu.co>
2024-05-05 01:36:45 -0500

    ADD Entrega2

--- Veterinaria/src/main/java/app/service/SellerService.java
+++
@@ -1,9 +0,0 @@
-package app.service;
-
-import app.dto.InvoiceDto;
-
-public interface SellerService {
-	public void createInvoice(InvoiceDto invoiceDto) throws Exception;
-
-	public void searchInvoice(InvoiceDto invoiceDto) throws Exception;
-}
